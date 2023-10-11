506244c1b7891cfde8ef035ae5e7cdf2c0f3ea55
Lucas Eduardo <lucas_spfc6@hotmail.com>
2023-10-10 23:30:45 -0300

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+
+    <div className="container">
+
+    <div>
+      <h1 className="title-text">Juba's Barbearia</h1>
+      <img
+        src={require('./assets/images/logoMarca.png')}
+        alt="Logo"
+        className="logo"
+      />
+    </div>
+
+      <form>
+
+        <div className="field-container">
+          <label htmlFor="cpf">Digite seu CPF:</label>
+          <input type="text" 
+            className="form-control" 
+            id="cpf" name="cpf" 
+            placeholder="123.456.789-10" 
+          required />
+        </div>
+        
+        <div className="password-container">
+          <label htmlFor="senha">Digite sua senha:</label>
+          <input type="password" 
+            className="form-control" 
+            id="senha" 
+            name="senha" 
+            placeholder="**********" 
+          required />
+        </div>
+
+        <div className="button-container">
+          <button className="btn btn-primary custom-button" type="button">Entrar</button>
+        </div>
+        
+        <p className="forgot-password">
+          <a href="#">Esqueci minha senha</a> 
+        
+          <p></p>
+
+          <a className="create-account"href="#">Não possui uma conta? Cadastre-se</a>
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </form>
     </div>
   );
 }
